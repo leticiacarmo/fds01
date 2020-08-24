cadf827948a9beef4acc31e9f6890e2c9f0b8ec3
kalissa <kdekalissa@hotmail.com>
2020-08-24 20:50:57 -0300

    Implementação catálogo

--- Catalogo.java
+++ Catalogo.java
@@ -1,4 +1,33 @@
 public class Catalogo{
-    private ArrayList<Veiculo> veiculos;
+    private ArrayList<Veiculo> carro;
+    private ArrayList<Veiculo> moto;
+    private ArrayList<Veiculo> caminhao;
 
+    private Catalogo() {
+        carro = new ArrayList<Veiculo>;
+        moto= new ArrayList<Veiculo>;
+        caminhao= new ArrayList<Veiculo>;
+    }
+
+    public void addCarro (Veiculo carro){
+        veiculos.add(carro);
+
+    }
+    public void addMoto (Veiculo moto){
+        veiculos.add(moto);
+
+    }
+    public void addCaminhao (Veiculo caminhao){
+        veiculos.add(caminhao);
+    }
+
+    public void removeCarro (Veiculo carro){
+        veiculos.remove(carro);
+
+    }public void removeMoto (Veiculo moto){
+        veiculos.remove(moto);
+
+    }public void removeCaminhao (Veiculo caminhao){
+        veiculos.remove(caminhao);
+    }
 }
