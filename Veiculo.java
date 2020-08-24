8531b1163bf287e512d6d302f489c101eea6d10f
kalissa <kdekalissa@hotmail.com>
2020-08-24 20:17:14 -0300

    Arrumando veiculo

--- Veiculo.java
+++ Veiculo.java
@@ -3,19 +3,6 @@ public Interface Veiculo{
     private String descricao;
     private double preco;
 
-    public int getPlaca() {
-        return placa;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-
     @Override
     public String toString() {
         return "Veiculo [placa=" + codigo + ", descricao=" + descricao + ", preço=" + preco"]";
