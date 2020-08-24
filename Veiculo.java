ffc2e755db14d4b9ec56a33af08ca7d6f0de5a6a
Letícia Bento do Carmo <leticiac@dbserver.com.br>
2020-08-23 22:17:45 -0300

    first commit

---
+++ Veiculo.java
@@ -0,0 +1,24 @@
+public Interface Veiculo{
+    private int placa;
+    private String descricao;
+    private double preco;
+
+    public int getPlaca() {
+        return placa;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Veiculo [placa=" + codigo + ", descricao=" + descricao + ", preço=" + preco"]";
+    }
+}
+    
